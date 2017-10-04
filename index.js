dcda7740eb70d078210dd6b54881fc87668ee830
Matthew Sandfrey <msandfrey@sbcglobal.net>
2017-10-04 21:57:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments){
+  var array
+  for(i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+}
+function johnLennonFacts(array){
+  i = 0
+  while(i < array.length){
+    array[i] = array[i] + "!!!"
+    i++
+  }
+  return array
+}
